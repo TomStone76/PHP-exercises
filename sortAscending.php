6e23ff939e309f04d92be5e769bfb7a7410cefee
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-09 23:24:06 -0500

    commit

---
+++ sortAscending.php
@@ -0,0 +1,11 @@
+<?php
+
+// Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+// Sort numbers array in ascending order.
+// If functions argument is null or empty array, return an empty array.
+// Return new array of sorted numbers.
+
+function sortNumsAscending($arr) {
+    
+}
