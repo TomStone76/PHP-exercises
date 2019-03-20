24ea3d590fa2565f725ab4cea38514f4665daad2
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-19 23:11:32 -0400

    partial solution

--- repeating.php
+++ repeating.php
@@ -1,22 +1,14 @@
 <?php
-function doubleChar($str) {
-    for ($i = 0, $n = explode('', $str).length; $i < $n; $i++) {
-        $newStr += str[i] + str[i];
-    }
-    return $newStr;
-}
-
-echo doubleChar("String");
-echo double
-
-
-
-// function doubleChar(str) {
-//     let newStr = '';
-//     for (let i = 0, n = str.split('').length; i < n ; i++) {
-//         newStr += str[i] + str[i];
+// function doubleChar($str) {
+//     for ($i = 0, $n = count(explode('', $str)); $i < $n; $i++) {
+//         $newStr += $str[$i] + $str[$i];
 //     }
-//     return newStr;
-// }
+//     return $newStr;
+// } 
 
-//explode(" ", $pizza);
+// echo doubleChar("String");
+// echo doubleChar("Hello World!");
+// echo doubleChar("1234!_ ");
+
+
+
