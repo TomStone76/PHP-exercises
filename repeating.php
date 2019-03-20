4918da7f67f8ba4e597a7d15cb7cf255718b380f
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-19 22:55:10 -0400

    set up function

---
+++ repeating.php
@@ -0,0 +1,15 @@
+<?php
+function doubleChar($str) {
+    $newStr = '';
+    for ($)
+}
+
+
+
+// function doubleChar(str) {
+//     let newStr = '';
+//     for (let i = 0, n = str.split('').length; i < n ; i++) {
+//         newStr += str[i] + str[i];
+//     }
+//     return newStr;
+// }
