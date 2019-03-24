a5accfe512c04078e1c04486d4bb172cab469a61
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-23 23:17:14 -0400

    commmit

--- xo.php
+++ xo.php
@@ -4,15 +4,14 @@
 
 function XO($str) {
     $numX = 0; $numO = 0; 
-    $lower = strtolower($str);
-    for ($i = 0; $i < $lower; $i++) {
-        if ($lower[$i] === 'x') {
-            $numX += 1;
-        } else if ($lower[$i] === 'o') {
-            $numO += 1;
-        }
+    $arr = explode(' ', $str);
+
+    for ($i = 0; $i < $arr; $i++) {
+        
     }
+
     return $numX === $numO;
+    return $arr;
 }
 
-echo XO('');
+echo XO('x');
