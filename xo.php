f3e2bc37fa0c034283eeb589da865acee307525c
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-23 22:57:47 -0400

    loop

---
+++ xo.php
@@ -0,0 +1,10 @@
+<?php
+// Create a function that takes a string, checks if it has the same number of
+// 'x's and 'o's and returns either true or false.
+
+function XO($str) {
+    $numX; $numO;
+    for ($i = 0; $i < $str; $i++) {
+        
+    }
+}
